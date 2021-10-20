811a86e25b9fc4a26d9a71d6b77a8271a07500e5
Ritabrata Bhattacharyya <ritabrata.bhattacharyya96@gmail.com>
2021-10-20 07:16:17 +0530

    Ritabrata_Assignment_1

--- src/ritabrata_bhattacharyya/Assignment_2.java
+++ src/ritabrata_bhattacharyya/Assignment_2.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.By.ByXPath;
+
 
 public class Assignment_2 {
 
