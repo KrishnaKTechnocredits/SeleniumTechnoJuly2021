935f8b882d52c6397a9f4bf7e1d756a8b39d9184
Priti Vattanavar <psvattanavar@gmail.com>
2021-10-24 19:24:56 +0530

    Priti_Assingment_2

---
+++ src/priti/Assingment_2/Assingment_2_1.java
@@ -0,0 +1,82 @@
+package priti.Assingment_2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Assingment_2_1 {
+	
+	void verifyTextOnAutomationPractice() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "./resources/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		System.out.println("Chrome Browser opened");
+		System.out.println("WebSite : " +"http://automationbykrishna.com/");
+		driver.get("http://automationpractice.com/");
+		
+		System.out.println("STEP-Verify the text: Women");
+		String womenText = driver.findElement(By.xpath("//a[@title='Women']")).getText();
+		String expectedText1= "WOMEN";
+		if (expectedText1.equals(womenText)) {
+			System.out.println("Pass");
+		}else {
+			System.out.println("Fail");
+		}
+		
+		System.out.println("STEP-Verify the text: Dresses");
+		String dressText= driver.findElement(By.xpath("//div[@id = 'block_top_menu']/ul[@class ='sf-menu clearfix menu-content sf-js-enabled sf-arrows']/li[2]//a [@title= 'Dresses']")).getText();
+		System.out.println(dressText);
+		String expectedText2 = "DRESSES";
+		if (expectedText2.equals(dressText)) {
+			System.out.println("Pass");
+		}else {
+			System.out.println("Fail");
+		}
+		
+		
+		System.out.println("STEP-Verify the text: T-Shirts");
+		String tshirtsText= driver.findElement(By.xpath("//div [@id ='block_top_menu']/ul [@class = 'sf-menu clearfix menu-content sf-js-enabled sf-arrows']/li[3]//a [@title = 'T-shirts']")).getText();
+		String expectedText3 = "T-SHIRTS";
+		if (expectedText3.equals(tshirtsText)) {
+			System.out.println("Pass");
+		}else {
+			System.out.println("Fail");
+		}
+		
+		System.out.println("STEP-Verify text on Sign in link");
+		String signinText = driver.findElement(By.xpath("//a[contains(text(), 'Sign in')]")).getText();
+		
+		if ("Sign in".equals(signinText)) {
+			System.out.println("Pass");
+		}else {
+			System.out.println("Fail");
+		}
+		
+		System.out.println("STEP-click on the Sing in");
+		driver.findElement(By.xpath("//a [@title ='Log in to your customer account']")).click();
+		Thread.sleep(2000);
+		System.out.println("STEP-Test case_2_1 executed successfully");
+		driver.close();
+	}
+		
+	public static void main(String []a) throws InterruptedException {
+		Assingment_2_1 assingment_2 = new Assingment_2_1();
+		assingment_2.verifyTextOnAutomationPractice();
+	}
+}
+/*Assignment - 2 : 16th Oct'2021
+Automate Singup : 
+http://automationpractice.com/index.php
+*/
+
+
+
+
+
+
+
+
+
+
+
