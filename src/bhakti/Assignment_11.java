bd7953c4475e2185d5101fa1664adca066cd9009
unknown <bskakade@gmail.com>
2021-10-26 16:44:42 +0530

    Assignmnet_11_all_programs_bk

--- src/bhakti/Assignment_11.java
+++ src/bhakti/Assignment_11.java
@@ -1,10 +1,22 @@
 /*Assginment - 11: 24th Oct'2021
 Program 1: Return unique Employees from table-2. 
-output : size - 8*/
+output : size - 8
+
+
+Program 2: print employee id which is duplicate in table-2 
+output : 76585 
+
+Program 3: print the name of deptment having maximum employees.
+output : 7013-IT
+
+
+*/
 
 package bhakti;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map.Entry;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -35,9 +47,10 @@ WebDriver driver;
 		System.out.println("End");
 		 driver.quit();
 	}
-	 //Return unique Employees from table-2. 
+	 //Program 1: Return unique Employees from table-2. . 
 	@Test
-	void findUniqueEmp() {
+	void p1_findUniqueEmp() {
+		System.out.println("Program 1: Return unique Employees from table-2. ");
 		int rows= driver.findElements(By.xpath("//table[@class='table table-striped']/tbody/tr")).size();
 		HashSet<String> empIDList= new HashSet();
 		HashSet<String> empID_NameList= new HashSet();
@@ -56,6 +69,58 @@ WebDriver driver;
 		int expected=8;
 		SoftAssert softAssert= new SoftAssert();
 		softAssert.assertEquals(actual, expected);
+		softAssert.assertAll();
 	}
+	//Program 2: print employee id which is duplicate in table-2 
+	@Test 
+	void findDuplicateEmpID() {
+		System.out.println("Program 2: print employee id which is duplicate in table-2");
+		int rows= driver.findElements(By.xpath("//table[@class='table table-striped']/tbody/tr")).size();
+		HashSet<String> empIDList= new HashSet();
+		String empName="";
+		for (int i=1; i<= rows; i++) {
+			String empID= driver.findElement(By.xpath("//table[@class='table table-striped']/tbody/tr["+i+"]/td[2]")).getText();
+			if (!empIDList.add(empID)) {
+				 empName= driver.findElement(By.xpath("//table[@class='table table-striped']/tbody/tr["+i+"]/td[3]")).getText();
+				System.out.println("Duplicate Employee-->" +empName); 
+			}
+		}
+		String expected= "Sumit Mate";
+		SoftAssert softAssert= new SoftAssert();
+		softAssert.assertEquals(empName, expected);
+		softAssert.assertAll();
+	}
+	//Program 3: print the name of deptment having maximum employees.:	output : 7013-IT
+	@Test
+	void deptNameWithMaxEMp() {
+		System.out.println("Program 3: print the name of deptment having maximum employees.:");
+		int rows= driver.findElements(By.xpath("//table[@class='table table-striped']/tbody/tr")).size();
+		HashMap<String, Integer> deptListMap= new HashMap();
+		for (int i=1; i<= rows; i++) {
+			String deptName=driver.findElement(By.xpath("//table[@class='table table-striped']/tbody/tr["+i+"]/td[5]")).getText();
+			if (deptListMap.containsKey(deptName)) {
+				int count=deptListMap.get(deptName);
+				deptListMap.put(deptName, ++count);
+				
+			}else {
+				deptListMap.put(deptName, 1);
+			}
 
+		}
+		System.out.println(deptListMap);
+		int max=0;
+		String maxDepName="";
+		for (Entry<String, Integer> deptName:deptListMap.entrySet() ) {
+			if (deptName.getValue()>max) {
+				max=deptName.getValue();
+				maxDepName= deptName.getKey();
+			}
+		}
+		System.out.println("Deparat having max employee-->"+maxDepName);
+		String expected= "7013-IT";
+		SoftAssert softAssert= new SoftAssert();
+		softAssert.assertEquals(maxDepName, expected);
+		softAssert.assertAll();
+		
+	}
 }
