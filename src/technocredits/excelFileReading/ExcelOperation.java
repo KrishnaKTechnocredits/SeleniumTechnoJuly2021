ec67ac6e6a8ec993a0990a03d696d7fb928b3b38
Harsh Vegada <harshhpatel07@gmail.com>
2021-11-03 09:17:38 +0530

    data provider code

--- src/technocredits/excelFileReading/ExcelOperation.java
+++ src/technocredits/excelFileReading/ExcelOperation.java
@@ -9,38 +9,57 @@ import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.testng.annotations.Test;
 
 public class ExcelOperation {
 
-	@Test
-	public void readExcelFile() throws IOException {
+	public static Object[][] readExcelFile() {
+
 		File file = new File("./excelFiles/testing.xlsx");// File Path
-		FileInputStream input = new FileInputStream(file);// File Not Exceltion
+		FileInputStream input;
+		Workbook workbook = null;
+		try {
+			input = new FileInputStream(file);
+			workbook = new XSSFWorkbook(input);// IO Exception
+		} catch (IOException e) {
+			e.printStackTrace();
+		} // File Not Exceltion
 
 		/**
 		 * 2003 -> XLS (HSSF) 2007 -> XLSX (XSSF)
 		 */
 		// Interface = class
-		Workbook workbook = new XSSFWorkbook(input);// IO Exception
 //		workbook= new HSSFWorkbook();
 		Sheet sheet = workbook.getSheet("Sheet1"); // import from ss pakage
 
-		int lastRow = sheet.getLastRowNum();
+		int lastRow = sheet.getLastRowNum() + 1;
 		int lastCell = sheet.getRow(0).getLastCellNum();
+
+		Object[][] data = new Object[lastRow][lastCell];
 
 		/**
 		 * For row it returns index instead of count
 		 */
 
-		System.out.println("Total Row: " + (lastRow + 1));
+		System.out.println("Total Row: " + (lastRow));
 		System.out.println("Total Cell: " + lastCell);
 
-		for (int row = 0; row <= lastRow; row++) {
+		for (int row = 0; row < lastRow; row++) {
 			for (int colums = 0; colums < lastCell; colums++) {
-				System.out.print(sheet.getRow(row).getCell(colums).getStringCellValue() + "\t");
+				Cell cell = sheet.getRow(row).getCell(colums);
+				if (cell.getCellType() == CellType.NUMERIC) {
+					data[row][colums] = String.valueOf(cell.getNumericCellValue());
+				} else if (cell.getCellType() == CellType.STRING) {
+					data[row][colums] = String.valueOf(cell.getStringCellValue());
+				} else if (cell.getCellType() == CellType.BOOLEAN) {
+					data[row][colums] = String.valueOf(cell.getBooleanCellValue());
+				}
 			}
-			System.out.println();
 		}
+		return data;
 	}
+
+//	public static void main(String[] args) throws IOException {
+//		readExcelFile();
+//	}
+
 }
