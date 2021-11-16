52c6d70e7a82cb06d7d8edd6a1e1cfffd1bad305
Margi Santoki <margisantoki@gmail.com>
2021-11-16 09:11:26 +0530

    Margi_SeleniumTest_1

---
+++ src/margi/test/Test_1.java
@@ -0,0 +1,65 @@
+package margi.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Test_1 {
+
+	WebDriver driver;
+
+	@BeforeClass
+	public void start() {
+		System.out.println("STEP : Launch chrome browser");
+		System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		System.out.println("STEP : Move to Snapdeal website");
+		driver.get("https://www.snapdeal.com/");
+	}
+
+	public ArrayList<String> menuTextList() {
+		System.out.println("STEP : Click on Not Now button");
+		driver.findElement(By.xpath("//button[@id='pushDenied']")).click();
+		
+		System.out.println("STEP : Move over on sign in button");
+		WebElement signIn = driver.findElement(By.xpath("//span[text()='Sign In']"));
+		Actions actions = new Actions(driver);
+		actions.moveToElement(signIn).perform();
+
+		System.out.println("STEP : Get menu text");
+		ArrayList<String> menuList = new ArrayList<String>();
+		List<WebElement> list = driver
+				.findElements(By.xpath("//div[contains(@class,'dropdownWrapper')]/div[2]/div/ul/li"));
+		for (int index = 0; index < list.size(); index++) {
+			menuList.add(list.get(index).getText());
+		}
+		return menuList;
+	}
+
+	@Test
+	public void testCase() {
+		System.out.println("STEP : Verify Menu items");
+		ArrayList<String> actualList = menuTextList();
+		ArrayList<String> expectedList = new ArrayList<String>();
+		expectedList.add("Your Account");
+		expectedList.add("Your Orders");
+		expectedList.add("Shortlist");
+		expectedList.add("SD Cash");
+		Assert.assertEquals(actualList, expectedList, "List not match");
+	}
+
+	@AfterClass
+	public void teardown() {
+		driver.close();
+	}
+}
