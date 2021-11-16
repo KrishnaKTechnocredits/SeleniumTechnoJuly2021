725b1857b20173c55414d4e4423619fa36a0c9dc
Monali Bendale <monaliborole31@gmail.com>
2021-11-16 10:59:37 +0530

    Added close browser method

--- src/monali/SeleniumTest_1/SeleniumTest_1_Snapdeal.java
+++ src/monali/SeleniumTest_1/SeleniumTest_1_Snapdeal.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -53,6 +54,11 @@ public class SeleniumTest_1_Snapdeal {
 		}
 		return actualList;
 		
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.close();
 	}
 	
 	
